62cb7b45b71c08c8561723e88d686cd13418c458
Frombolas <guillones11@gmail.com>
2025-02-10 15:27:46 -0300

    #01 mudanças nas classes gasto e usuario

--- src/model/Gasto.java
+++ src/model/Gasto.java
@@ -16,32 +16,17 @@ public class Gasto {
         this.usuario = usuario;
     }
 
-    public double getValor() {
-        return valor;
-    }
-
     public void setValor(double valor) {
         this.valor = valor;
     }
 
-    public String getCategoria() {
-        return categoria;
-    }
 
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
 
-    public LocalDate getData() {
-        return data;
-    }
-
     public void setData(LocalDate data) {
         this.data = data;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
     }
 
     public void setUsuario(Usuario usuario) {
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,30 +1,38 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class Usuario {
 
     private String nome;
     private String tipo;
+    private Gasto[] gastos;
 
     public Usuario(String nome, String tipo) {
         this.nome = nome;
         this.tipo = tipo;
-    }
-
-    public String getNome() {
-        return nome;
+        gastos = new Gasto[1];
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getTipo() {
-        return tipo;
-    }
-
     public void setTipo(String tipo) {
         this.tipo = tipo;
+    }
+
+    public void setGastos(Gasto[] gastos) {
+        this.gastos = gastos;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", gastos=" + Arrays.toString(gastos) +
+                '}';
     }
 }
