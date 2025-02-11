060a70ffc86c3d4bcb742e1ed2caaea5ea0811fe
Frombolas <guillones11@gmail.com>
2025-02-11 14:23:52 -0300

    #6 inicio do CRUD da classe gasto e teste com Arquivos

--- src/dao/GastoDao.java
+++ src/dao/GastoDao.java
@@ -1,5 +1,54 @@
 package dao;
 
+import model.Gasto;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CancellationException;
+
 public class GastoDao {
+    private File arquivo;
+
+    public GastoDao() {
+        arquivo = new File("Gastos");
+
+        if(!arquivo.exists()) {
+            try {
+                arquivo.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+    private void atualizarArquivo(Set<Gasto> gasto) throws IOException {
+        try (ObjectOutputStream out = new ObjectOutputStream(
+                new FileOutputStream(arquivo))) {
+            out.writeObject(gasto);
+        }
+    }
+
+    public Set<Gasto> getGastos() throws IOException, ClassNotFoundException {
+        if(arquivo.length()== 0){
+            return new HashSet<>();
+        }
+
+        try(ObjectInputStream in = new ObjectInputStream(
+                new FileInputStream(arquivo))) {
+            return (Set<Gasto>) in.readObject();
+        }
+    }
+
+    public boolean adicionarGasto(Gasto gasto) throws IOException,ClassNotFoundException {
+        Set<Gasto> gastos = getGastos();
+        if(gastos.add(gasto)){
+            atualizarArquivo(gastos);
+            return true;
+        }
+        return false;
+    }
+
 
 }
--- src/model/Gasto.java
+++ src/model/Gasto.java
@@ -1,25 +1,28 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Gasto {
 
+    private final long serialVersionUID;
     private double valor;
     private String categoria;
     private LocalDate data;
     private Usuario usuario;
 
-    public Gasto(double valor, String categoria, LocalDate data, Usuario usuario) {
+    public Gasto(long serialVersionUID, double valor, String categoria, LocalDate data, Usuario usuario) {
+        this.serialVersionUID = 1L;
         this.valor = valor;
         this.categoria = categoria;
         this.data = data;
         this.usuario = usuario;
     }
 
+
     public void setValor(double valor) {
         this.valor = valor;
     }
-
 
     public void setCategoria(String categoria) {
         this.categoria = categoria;
@@ -31,5 +34,29 @@ public class Gasto {
 
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Gasto{" +
+                "serialVersionUID=" + serialVersionUID +
+                ", valor=" + valor +
+                ", categoria='" + categoria + '\'' +
+                ", data=" + data +
+                ", usuario=" + usuario +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gasto gasto = (Gasto) o;
+        return serialVersionUID == gasto.serialVersionUID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(serialVersionUID);
     }
 }
